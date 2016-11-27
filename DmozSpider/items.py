91111ff400203887d3a93de1eaf0f3acbde36b81
xiphodon <827789895@qq.com>
2016-11-27 19:45:52 +0800

    爬取dmoz网站中的两个网页，并将爬取结果保存在json中

--- DmozSpider/items.py
+++ DmozSpider/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class DmozspiderItem(scrapy.Item):
+class DmozSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
--- DmozSpider/spiders/Dmoz_spider.py
+++ DmozSpider/spiders/Dmoz_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from DmozSpider.items import DmozSpiderItem
 
 class DmozSpider(scrapy.Spider):
 	# 爬虫名字（唯一）
@@ -14,6 +15,18 @@ class DmozSpider(scrapy.Spider):
 	def parse(self,response):
 		# 解析器，获取链接下载后的响应数据
 
-		filename = response.url.split("/")[-2]
-		with open(filename + ".txt","wb") as f:
-			f.write(response.body)
+		# filename = response.url.split("/")[-2]
+		# with open(filename + ".txt","wb") as f:
+		# 	f.write(response.body)
+
+		sel = scrapy.selector.Selector(response)
+		sites = sel.xpath('//div[@class="title-and-desc"]')
+		items = []
+		for site in sites:
+			item = DmozSpiderItem()
+			item['title'] = site.xpath('a/div[@class="site-title"]/text()').extract()
+			item['des'] = site.xpath('div[@class="site-descr"]/text()').extract()
+			item['url'] = site.xpath('a/@href').extract()
+			items.append(item)
+
+		return items
